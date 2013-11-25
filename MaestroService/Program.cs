86e62810324382fe22570ee048889ef097a84e21
Allen Lyons <allen.lyons@gmail.com>
2013-11-25 12:22:50 -0500

    no more precompile directives for testing

--- MaestroService/Program.cs
+++ MaestroService/Program.cs
@@ -9,22 +9,26 @@ namespace MaestroService
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
+
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[] 
             { 
                 new MaestroService() 
             };
 
-#if !SERVICE_DEBUG
-            ServiceBase.Run(ServicesToRun);
-#else
-           Log.Debug("Starting in test mode.");
-           ((MaestroService) ServicesToRun[0]).Start();
-           while (true)
-               Thread.Sleep(1000);
-#endif
+            if (args.Length == 0)
+            {
+                ServiceBase.Run(ServicesToRun);
+            }
+            else
+            {
+                Log.Debug("Starting in test mode.");
+                ((MaestroService)ServicesToRun[0]).Start();
+                while (true)
+                    Thread.Sleep(1000);
+            }
         }
     }
 }
